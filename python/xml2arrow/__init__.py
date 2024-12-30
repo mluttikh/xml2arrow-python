4168909410096c9769bfb3261018a370e76cb817
Mark <mluttikh@gmail.com>
2024-12-30 16:37:50 +0100

    feat: Expose exceptions (#5)

--- python/xml2arrow/__init__.py
+++ python/xml2arrow/__init__.py
@@ -1,3 +1,4 @@
 """This module provides functionality to parse XML files into Arrow tables based on a configuration."""
 
-from ._xml2arrow import *
+from . import exceptions
+from ._xml2arrow import XmlToArrowParser
