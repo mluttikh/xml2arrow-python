a0a20a0c3df539f75a5811aa61886155b94d8408
Mark <mluttikh@gmail.com>
2026-02-01 18:44:37 +0100

    style: Add Ruff linter (#23)

--- python/xml2arrow/__init__.py
+++ python/xml2arrow/__init__.py
@@ -1,4 +1,4 @@
-"""This module provides functionality to parse XML files into Arrow tables based on a configuration."""
+"""Parse XML files into Arrow tables using a YAML configuration."""
 
 from . import exceptions
 from ._xml2arrow import Xml2ArrowError, XmlToArrowParser, _get_version
